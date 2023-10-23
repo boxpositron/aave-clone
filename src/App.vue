b69f698c346a42a8ca4ff57dd64dc02918bc2f0d
David Ibia <david.ibia@boxmarshall.com>
2023-10-23 03:51:13 +0100

    feat(App.vue): add App.vue component with template, script, and styles to create the main structure of the Aave app feat(App.vue): add template with page-header, view, and page-footer components to create the layout of the app feat(App.vue): add script setup with imports for PageHeader, PageFooter, and LandingPage components to enable their usage in the template feat(App.vue): add styles to define the layout and styling of the Aave app

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="aave-app">
+    <page-header></page-header>
+    <div class="view">
+      <landing-page></landing-page>
+    </div>
+    <page-footer></page-footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import PageHeader from "./components/window/PageHeader.vue";
+import PageFooter from "./components/window/PageFooter.vue";
+import LandingPage from "./views/LandingPage.vue";
+</script>
+
+<style lang="scss">
+@import "./assets/scss/default.scss";
+</style>
+
+<style lang="scss" scoped>
+div.aave-app {
+  display: flex;
+  flex-direction: column;
+
+  div.view {
+    margin-top: 60px;
+    min-height: 100vh;
+  }
+}
+</style>
