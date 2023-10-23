3eb12d3cc9e1ab15bb9e400ae62b5a4c7494be23
David Ibia <david.ibia@boxmarshall.com>
2023-10-23 03:50:07 +0100

    feat(PageHeader.vue): add PageHeader component to provide a consistent header across pages
    The PageHeader.vue file contains the implementation of the PageHeader component. This component is responsible for rendering the header section of the application. It includes a navigation menu with links to different sections of the app, a brand logo, and a launch app button.
    
    The component is implemented using Vue.js and TypeScript. It uses a template to define the structure of the header, a script section for any logic, and a scoped style section for styling.
    
    The header is positioned fixed at the top of the page and has a height of 60px. It has a background color of $athens-gray and is divided into three sections: left, middle, and right.
    
    The left section contains the brand logo, which is displayed as "AAVE" in a span with the class "brand-text".
    
    The middle section contains the navigation menu, which is implemented as an unordered list with the class "nav-link-container". Each list item represents a navigation link, and the links are styled with the class "page-navigation".
    
    The right section contains an action button with the class "action primary". This button is used to launch the app and has a background color of $bright-gray and text color of $athens-gray-dim.
    
    The component is scoped, meaning that the styles defined within the component only apply to the component itself and do not affect other elements on the page.
    
    This component is added to provide a consistent header across pages and improve the overall user experience of the application.

---
+++ src/components/window/PageHeader.vue
@@ -0,0 +1,113 @@
+<template>
+  <nav>
+    <div class="left">
+      <div class="brand">
+        <span class="brand-text"> AAVE </span>
+      </div>
+    </div>
+    <div class="middle">
+      <div class="links">
+        <ul class="nav-link-container">
+          <li class="nav-link">
+            <a href="#protocol" class="page-navigation">Aave Protocol</a>
+          </li>
+          <li class="nav-link">
+            <a href="#governance" class="page-navigation">Governance</a>
+          </li>
+          <li class="nav-link">
+            <a href="#docs" class="page-navigation">Docs</a>
+          </li>
+          <li class="nav-link">
+            <a href="#security" class="page-navigation">Security</a>
+          </li>
+          <li class="nav-link">
+            <a href="#faq" class="page-navigation">FAQ</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="right">
+      <div class="action">
+        <button class="action primary">Launch App</button>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import "../../assets/scss/default.scss";
+@import "../../assets/scss/colors.scss";
+
+nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  right: 0;
+  height: 60px;
+
+  background-color: $athens-gray;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  gap: 50px;
+
+  padding: 0 30px;
+
+  div.left {
+    div.brand {
+      span.brand-text {
+        font-size: 2.5rem;
+        font-weight: 300;
+      }
+    }
+  }
+
+  div.middle {
+    div.links {
+      ul.nav-link-container {
+        display: flex;
+        flex-direction: row;
+        -webkit-padding-start: 0;
+        padding: 0;
+        margin: 0;
+
+        gap: 40px;
+        list-style: none;
+
+        li {
+          a {
+            text-decoration: none;
+            color: $bright-gray;
+            font-weight: 500;
+          }
+        }
+      }
+    }
+  }
+
+  div.right {
+    div.action {
+      button.action {
+        border: none;
+
+        height: 35px;
+        border-radius: 5px;
+        font-size: .95rem;
+        font-weight: 400;
+        padding: 0 10px;
+
+        &.primary {
+          background-color: $bright-gray;
+          color: $athens-gray-dim;
+        }
+      }
+    }
+  }
+}
+</style>
