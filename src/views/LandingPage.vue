f6e399556cb37293d6252ec21ef9317bc9acda35
David Ibia <david.ibia@boxmarshall.com>
2023-10-23 03:49:58 +0100

    feat(LandingPage.vue): add LandingPage component with a hero section
    The LandingPage component is added with a hero section. The hero section has a background image that is a linear gradient from the fuchsia-pink color to the pelorous color. The minimum height of the hero section is set to 70vh.

---
+++ src/views/LandingPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="landing-page">
+    <section class="hero"></section>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@import "../assets/scss/colors.scss";
+
+div.landing-page {
+  section {
+    &.hero {
+      background-image: linear-gradient(45deg, $fuchsia-pink, $pelorous);
+      min-height: 70vh;
+    }
+  }
+}
+</style>
